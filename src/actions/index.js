b3f0e530fe273519e4b8bd920418815c2f49e73b
Melissa Nguyen <nguyen.melissa23@gmail.com>
2021-01-06 11:32:14 -0800

    completed integration test

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,7 +11,7 @@ export function saveComment(comment) {
 export async function fetchComments()  {
 	const response = await axios.get('http://jsonplaceholder.typicode.com/comments');
 	const comments = response.data.map((comment) => comment.name);
-	console.log(comments);
+	// console.log(comments);
 	return {
 		type: FETCH_COMMENTS, 
 		payload: comments
--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -14,7 +14,7 @@ afterEach(() => {
 });
 
 
-it('has a text area and a button', () => {
+it('has a text area and two buttons', () => {
 	expect(wrapped.find('textarea').length).toEqual(1);
 	expect(wrapped.find('button').length).toEqual(2);
 });
